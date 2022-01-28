d74dbb9d8adafc9eb806360f92fc990b7a9b7359
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-27 18:14:57 -0800

    update

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const corsOptions = {
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,6 +20,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,7 +38,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/shop?retryWrites=true')
+mongoose.connect(MONGODB_URL)
 
   .then(result => {
     User.findOne().then(user => {
@@ -48,8 +53,11 @@ mongoose.connect('mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/shop?
         user.save();
       }
     });
-    app.listen(5000);
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
   });
+
+
+  
