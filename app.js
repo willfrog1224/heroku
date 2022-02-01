98ce6657d60e7cfba34e3b9194d7df1e6bf33dbe
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-31 17:35:11 -0800

    make it better

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const cors = require('cors');
+
+const MONGODB_URI =
+  'mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
 const corsOptions = {
   origin: "https://jacse341-main.herokuapp.com/",
   optionsSuccessStatus: 200
 };
+  
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,12 +33,22 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('61f0c4fae27c3373eafb5fee')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -34,16 +56,16 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use(cors(corsOptions));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(MONGODB_URL)
-
+mongoose
+  .connect(MONGODB_URI)
   .then(result => {
-    console.log("connected to mongo searching for user")
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
@@ -61,6 +83,3 @@ mongoose.connect(MONGODB_URL)
   .catch(err => {
     console.log(err);
   });
-
-
-  
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,29 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[1]
-    .trim()
-    .split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true');
-  res.redirect('/');
+  User.findById('61f0c4fae27c3373eafb5fee')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
